2244d7cc520bf38d5674b467570f1cb8284207c3
RobertoPSF <88672689+RobertoPSF@users.noreply.github.com>
2021-09-03 17:41:21 -0300

    Update InsertionAlfabetico.java

--- InsertionAlfabetico.java
+++ InsertionAlfabetico.java
@@ -13,6 +13,9 @@ public class InsertionAlfabetico extends Escrever{
         }
         long totalTime = System.nanoTime() - startTime;
         System.out.println("InsertionSort para ordem alfabetica para o caso médio: " + (totalTime) + " milisegundos");
+        double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
         String path = "iSort_ordena_cidades_medio.csv";
         escreverCsv(lista, path);
         insertionSortAlfabeticaMelhor(lista);
@@ -32,6 +35,9 @@ public class InsertionAlfabetico extends Escrever{
         }
         long totalTime = System.nanoTime() - startTime;
         System.out.println("InsertionSort para ordem alfabetica para o melhor caso: " + (totalTime) + " milisegundos");
+        double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
         String path = "iSort_ordena_cidades_melhor.csv";
         escreverCsv(lista, path);
         insertionSortAlfabeticaPior(lista);
@@ -51,6 +57,9 @@ public class InsertionAlfabetico extends Escrever{
         }
         long totalTime = System.nanoTime() - startTime;
         System.out.println("InsertionSort para ordem alfabetica para o pior caso: " + (totalTime) + " milisegundos");
+        double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
         String path = "iSort_ordena_cidades_pior.csv";
         escreverCsv(lista, path);
 	}
