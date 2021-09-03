7aa195c77ae61fc03025543941106c1d084501a7
RobertoPSF <88672689+RobertoPSF@users.noreply.github.com>
2021-09-03 17:44:34 -0300

    Update SelectionCasos.java

--- SelectionCasos.java
+++ SelectionCasos.java
@@ -17,9 +17,12 @@ public class SelectionCasos extends Escrever{
 		}
 		long totalTime = System.nanoTime() - startTime;
         System.out.println("SelectionSort para quantidade de casos acumulados para o caso médio: " + (totalTime) + " milisegundos");
-		String path = "sSort_ordena_casos_medio.csv";
+	double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
+        String path = "sSort_ordena_casos_medio.csv";
         escreverCsv(array, path);
-		selectionSortCasosMelhorCaso(array);
+	selectionSortCasosMelhorCaso(array);
 	}
 
 	public static void selectionSortCasosMelhorCaso(Dados[] array) {
@@ -40,9 +43,12 @@ public class SelectionCasos extends Escrever{
 		}
 		long totalTime = System.nanoTime() - startTime;
         System.out.println("SelectionSort para quantidade de casos acumulados para o melhor caso: " + (totalTime) + " milisegundos");
-		String path = "sSort_ordena_casos_melhor.csv";
+	double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
+        String path = "sSort_ordena_casos_melhor.csv";
         escreverCsv(array, path);
-		selectionSortCasosPiorCaso(array);
+	selectionSortCasosPiorCaso(array);
 	}
 
 	public static void selectionSortCasosPiorCaso(Dados[] array) {
@@ -63,7 +69,10 @@ public class SelectionCasos extends Escrever{
 		}
 		long totalTime = System.nanoTime() - startTime;
         System.out.println("SelectionSort para quantidade de casos acumulados parao pior caso: " + (totalTime) + " milisegundos");
-		String path = "sSort_ordena_casos_pior.csv";
+	double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
+        String path = "sSort_ordena_casos_pior.csv";
         escreverCsv(array, path);
 	}
 }
