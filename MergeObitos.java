5e10f91aa4044970f8f0076c748abd605bfa6f14
RobertoPSF <88672689+RobertoPSF@users.noreply.github.com>
2021-09-03 17:42:22 -0300

    Update MergeObitos.java

--- MergeObitos.java
+++ MergeObitos.java
@@ -7,6 +7,9 @@ public class MergeObitos extends Escrever {
 
         long totalTime = System.nanoTime() - startTime;
         System.out.println("Merge para ordem óbitos acumulados para o caso médio: " + (totalTime) + " milisegundos");
+        double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
         String path = "mSort_ordena_obitos_medio.csv";
         escreverCsv(lista, path);
         mergeSortObitosMelhor(lista);
@@ -51,6 +54,9 @@ public class MergeObitos extends Escrever {
 
         long totalTime = System.nanoTime() - startTime;
         System.out.println("Merge para ordem óbitos acumulados para o melhor caso: " + (totalTime) + " milisegundos");
+        double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
         String path = "mSort_ordena_obitos_melhor.csv";
         escreverCsv(lista, path);
         mergeSortObitosPior(lista);
@@ -95,6 +101,9 @@ public class MergeObitos extends Escrever {
 
         long totalTime = System.nanoTime() - startTime;
         System.out.println("Merge para ordem óbitos acumulados para o pior caso: " + (totalTime) + " milisegundos");
+        double memoriaUtilizada = ( (double)((double)(Runtime.getRuntime().totalMemory()/1024)/1024))- ((double)((double)(Runtime.getRuntime().freeMemory()/1024)/1024));
+        System.out.printf("Memória utlizada: %.2f Mb\n", memoriaUtilizada);
+        System.out.printf("Memória utlizada: %.3f Gb\n", memoriaUtilizada/1024);
         String path = "mSort_ordena_obitos_pior.csv";
         escreverCsv(lista, path);
     } 
